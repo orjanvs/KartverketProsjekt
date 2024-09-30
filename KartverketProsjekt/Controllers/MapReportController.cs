749f32060dd1195c607c44bd1e37e902be37f2df
Benjamin Papp <benjamin.thorsen.papp@gmail.com>
2024-09-30 14:11:48 +0200

    HttpPost actionmetode for innmelding. Per naa kun beskrivelse, ingen geojson-implementasjon.

--- KartverketProsjekt/Controllers/MapReportController.cs
+++ KartverketProsjekt/Controllers/MapReportController.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using KartverketProsjekt.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
 
 namespace KartverketProsjekt.Controllers
 {
     public class MapReportController : Controller
     {
+        private static List<MapReportModel> _mapReports = new List<MapReportModel>();
         public IActionResult AddForm()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult AddForm(MapReportModel mapReport)
+        {
+            var newMapReport = new MapReportModel
+            {
+                MapReportId = _mapReports.Count + 1,
+                Description = mapReport.Description,
+                Category = mapReport.Category,
+                GeoJson = mapReport.GeoJson,
+                Attachment = mapReport.Attachment,
+                CaseStatus = "Open",
+                SubmissionDate = DateOnly.FromDateTime(DateTime.Now)
+            };
+
+            _mapReports.Add(newMapReport);
+            return RedirectToAction("AddForm");
         }
     }
 }
