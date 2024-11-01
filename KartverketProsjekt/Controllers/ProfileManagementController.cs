41e7ebe6b413e1e346f5c9efefd2e60db73fdef1
DAkintola94 <degameros@gmail.com>
2024-11-01 15:26:55 +0100

    html for å lage bruker, registrerbruker laget

---
+++ KartverketProsjekt/Controllers/ProfileManagementController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KartverketProsjekt.Controllers
+{
+    public class ProfileManagementController : Controller
+    {
+        public IActionResult CreateAccount()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult LogOut()
+        {
+            return View();
+        }
+    }
+}
