f218d46a6c8c6dd826666de7a9d86301c3e58d8c
terjems <terjems@uia.no>
2024-11-08 17:58:35 +0100

    Added profilepage

---
+++ KartverketProsjekt/Models/ViewModels/ProfilePageViewModel.cs
@@ -0,0 +1,9 @@
+namespace KartverketProsjekt.Models.ViewModels
+{
+    public class ProfilePageViewModel
+    {
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
