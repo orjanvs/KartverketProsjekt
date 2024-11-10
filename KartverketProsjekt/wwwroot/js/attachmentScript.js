7d4675ac5e7aea4630abadee0301b945546f0e64
orjanvs <orjanvs@uia.no>
2024-11-10 13:52:31 +0100

    real time title preview i egen JS fil

--- KartverketProsjekt/wwwroot/js/attachmentScript.js
+++ KartverketProsjekt/wwwroot/js/attachmentScript.js
@@ -1,18 +1,4 @@
-﻿// Adding real-time title preview functionality
-document.addEventListener("DOMContentLoaded", function () {
-    var titleInput = document.getElementById('title');
-    var titlePreview = document.getElementById('form-title-preview');
-
-    titleInput.addEventListener('input', function () {
-        var currentTitle = titleInput.value;
-        if (currentTitle.length > 0) {
-            titlePreview.innerText = "Ny innmelding - " + currentTitle;
-        } else {
-            titlePreview.innerText = "Ny innmelding";
-        }
-    });
-});
-
+﻿
 document.getElementById('add-attachment').addEventListener('click', function () {
     // Create a new input element for file upload
     var newAttachmentInput = document.createElement('input');
