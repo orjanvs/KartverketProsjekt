ff37023fd77358c4e3a0049f75ec154c5e3664d5
orjanvs <orjanvs@uia.no>
2024-11-10 19:24:12 +0100

    null merker

--- KartverketProsjekt/Models/ViewModels/ViewMapReportRequest.cs
+++ KartverketProsjekt/Models/ViewModels/ViewMapReportRequest.cs
@@ -5,29 +5,29 @@ namespace KartverketProsjekt.Models.ViewModels
     public class ViewMapReportRequest
     {
         public int MapReportId { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
         public int MapLayerId { get; set; }
-        public string MapLayerType { get; set; }
-        public string GeoJsonString { get; set; }
+        public string? MapLayerType { get; set; }
+        public string? GeoJsonString { get; set; }
         public int MapReportStatusId { get; set; }
-        public string StatusDescription { get; set; }
+        public string? StatusDescription { get; set; }
         public DateTime SubmissionDate { get; set; }
-        public string County { get; set; }
-        public string Municipality { get; set; }
+        public string? County { get; set; }
+        public string? Municipality { get; set; }
 
 
-        public string SubmitterId { get; set; } 
-        public string SubmitterName { get; set; }
+        public string? SubmitterId { get; set; } 
+        public string? SubmitterName { get; set; }
 
         public string? CaseHandlerId { get; set; }
         public string? CaseHandlerName { get; set; }
 
 
 
-        public ICollection<AddAttachmentRequest> Attachments { get; set; }
+        public ICollection<AddAttachmentRequest>? Attachments { get; set; }
 
-        public List<SelectListItem> AvailableCaseHandlers { get; set; }
+        public List<SelectListItem>? AvailableCaseHandlers { get; set; }
 
 
     }
